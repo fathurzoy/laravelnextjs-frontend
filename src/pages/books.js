f38f5b57f0f6c6f2cc2ef7c5848b9d6f7e4915e1
Fathur Rahman <fathur.rahman@udana.id>
2022-03-14 21:09:04 +0700

    React Clean Code & Custom Hooks

--- src/pages/books.js
+++ src/pages/books.js
@@ -1,3 +1,4 @@
+import useBook from '@/components/book/customHooks'
 import BookForm from '@/components/book/form'
 import BookList from '@/components/book/list'
 import AppLayout from '@/components/Layouts/AppLayout'
@@ -5,25 +6,8 @@ import axios from '@/lib/axios'
 import { useFormik } from 'formik'
 import Head from 'next/head'
 import React, { useEffect, useState } from 'react'
-import * as Yup from 'yup'
-
+import { bookSchema } from '@/components/book/schema'
 const BookPage = () => {
-    const [books, setBooks] = useState([])
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState(null)
-
-    const bookSchema = Yup.object().shape({
-        name: Yup.string()
-            .min(4, 'Minimal 4 character!')
-            .max(254, 'Maximum 254 character!')
-            .required('Name is required'),
-        description: Yup.string()
-            .min(10, 'Minimal 10 character!')
-            .max(300, 'Maximal 300 character!')
-            .required('Description is required'),
-        price: Yup.number().required('Price is required'),
-    })
-
     const formik = useFormik({
         initialValues: {
             name: '',
@@ -36,23 +20,10 @@ const BookPage = () => {
             // handleSubmit(values, resetForm)
             try {
                 if (values.id) {
-                    const { data } = await axios.put(
-                        `http://localhost:8000/api/books/${values.id}`,
-                        values,
-                    )
-
                     console.log(data)
-                    handleUpdateBooks({
-                        book: data.data,
-                    })
+                    handleUpdateBooks(values)
                 } else {
-                    const { data } = await axios.post(
-                        'http://localhost:8000/api/books',
-                        values,
-                    )
-                    handleAddBook({
-                        book: data.data,
-                    })
+                    handleAddBook(values)
                 }
 
                 resetForm()
@@ -62,68 +33,17 @@ const BookPage = () => {
         },
     })
 
-    const fetchBooks = async () => {
-        try {
-            setLoading(true)
-            const { data } = await axios.get('http://localhost:8000/api/books')
+    const {
+        books,
+        bookLoading,
+        bookError,
+        getBook,
+        handleAddBook,
+        handleUpdateBooks,
+        handleDeleteBook,
+    } = useBook(formik)
 
-            setBooks(data.data)
-        } catch (error) {
-            setError(error.message)
-        } finally {
-            setLoading(false)
-        }
-    }
-
-    useEffect(() => {
-        fetchBooks()
-    }, [])
-
-    const getBook = async id => {
-        try {
-            const { data } = await axios.get(
-                `http://localhost:8000/api/books/${id}`,
-            )
-
-            const book = data.data
-
-            formik.setFieldValue('name', book.name)
-            formik.setFieldValue('description', book.description)
-            formik.setFieldValue('price', book.price)
-            formik.setFieldValue('id', book.id)
-            console.log(data)
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    const handleAddBook = ({ book }) => {
-        setBooks(prev => [...prev, book])
-    }
-
-    const handleUpdateBooks = ({ book }) => {
-        const updateBooks = books.map(item =>
-            item.id === book.id ? book : item,
-        )
-
-        setBooks(updateBooks)
-    }
-
-    const handleDeleteBook = async id => {
-        const isOK = confirm('Are you sure want to delete this data?')
-        if (isOK) {
-            try {
-                await axios.delete(`http://localhost:8000/api/books/${id}`)
-                const filteredBooks = books.filter(item => item.id !== id)
-
-                setBooks(filteredBooks)
-            } catch (error) {
-                console.log(error)
-            }
-        }
-    }
-
-    if (error) return error
+    if (bookError) return bookError
 
     return (
         <AppLayout
